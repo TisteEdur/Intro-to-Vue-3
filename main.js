f51d8e0d4949ef0e24cb9061e5ede036c8cc9b51
Anton Grigorov <doz3r@abv.bg>
2023-12-21 22:58:57 +0200

    L10-exercise

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItem(id) {
+            // My solution
+            // this.cart.pop(id)
+
+            // Course solution
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                this.cart.splice(index, 1)
+            }
+        }
+    }
 })
